6431de2f88d3fa4a619b5335c2df0bc460465a5c
P@Cm4n <verigindanya@yandex.ru>
2025-10-01 17:38:31 +0300

    Complete clickListener on button

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Нажат поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        val buttonMediateka = findViewById<Button>(R.id.mediateka)
+        buttonMediateka.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата медиатека!", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+        val buttonSettings = findViewById<Button>(R.id.settings)
+        buttonSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажаты настройки!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
