a2d17afbbd57fb46e956a07d1a8bcf8c65c1f67b
P@Cm4n <verigindanya@yandex.ru>
2025-12-15 19:35:12 +0300

    tasks: add activity audiopleer

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.os.Parcelable
 import java.util.UUID
 
 data class Track(
@@ -8,5 +9,11 @@ data class Track(
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: Long,
-    val artworkUrl100: String
-)
+    val artworkUrl100: String?,
+    val collectionName: String?,
+    val releaseDate: String?,
+    val primaryGenreName: String?,
+    val country: String?
+) {
+    fun getCoverArtwork() = artworkUrl100?.replaceAfterLast("/", "512x512bb.jpg")
+}
